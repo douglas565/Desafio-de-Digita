43033056be29f15eae52077566a44c0357a55330
Grots <drcharqueiro@inf.ufsm.br>
2023-12-02 19:57:58 -0300

    comit

--- T2/t2.c
+++ T2/t2.c
@@ -14,6 +14,15 @@
 
 #define TEMPO 20
 #define TOTAL_DE_PALAVRAS 10
+#define ponts_max 100
+#define descont_l_errada 10
+
+typedef struct
+{
+  float pontos;
+  char identificador[16];
+
+} recorde;
 
 typedef struct
 {
@@ -26,12 +35,13 @@ typedef struct
 } palavras;
 
 palavras vetPalavras[10];
+recorde recordes[3];
 
 void reorganiza_matriz(char palavras[10][16], int index_acertado);
 void remove_letra(char v[], int pos);
 void apresentacao();
 void encerramento();
-void mostra_matriz(double tempo_restante, int indice, double inicio);
+void mostrar_tela(double tempo_restante, int indice, double inicio);
 bool quer_jogar_de_novo();
 void preenche_vet(char v[], int min_len, int max_len);
 void espera_enter();
@@ -130,9 +140,10 @@ void popular_matriz()
   }
 }
 
-void mostra_matriz(double tempo_restante, int indice, double inicio)
+void mostrar_tela(double tempo_restante, int indice, double inicio)
 {
   tela_limpa();
+  float pontos = 0;
   int i;
   int lin;
   int col;
@@ -170,6 +181,11 @@ void mostra_matriz(double tempo_restante, int indice, double inicio)
   tela_lincol(0, 0);
   printf("digite as palavras conforme aparecem:");
   tela_atualiza();
+
+  tela_cor_normal();
+  tela_lincol(0, 0);
+  printf("Pontos: %.2f", pontos); // Mostra a pontuação na tela
+  tela_atualiza();
 }
 
 int encontra_palavra_com_letra(char letra, int posicao)
@@ -177,17 +193,31 @@ int encontra_palavra_com_letra(char letra, int posicao)
   for (int i = 0; i < TOTAL_DE_PALAVRAS; i++)
   { // varre todas a linhas da matriz
     if (vetPalavras[i].palavra[0] == letra && vetPalavras[i].ativa == true)
-    { 
+    {
       return i;
     }
   }
   return -1;
 }
 
+float calcular_pontos(double tempo_atual, double tempo_anterior)
+{
+  float tempo_diff = tempo_atual - tempo_anterior;
+  if (tempo_diff >= 1.0)
+  {
+    return 1.0;
+  }
+  else
+  {
+    return ponts_max * (1 - tempo_diff);
+  }
+}
+
 void jogo()
 {
   int quantidade_de_palavras_acertadas = 0;
   int quantidade_de_palavras = 10; // Novo controle para a quantidade de palavras na matriz.
+  float pontos = 0;
 
   popular_matriz(vetPalavras);
   long t0 = tela_relogio();
@@ -199,16 +229,16 @@ void jogo()
   while (quantidade_de_palavras_acertadas <= TOTAL_DE_PALAVRAS)
   {
     printf("%d  %d\n", quantidade_de_palavras_acertadas, TOTAL_DE_PALAVRAS);
-    
+
     resta = TEMPO - (tela_relogio() - t0);
 
-    mostra_matriz(resta, palavra_selecionada, t0); // Mostra todas as palavras na tela
+    mostrar_tela(resta, palavra_selecionada, t0); // Mostra todas as palavras na tela
 
     char letra = tecla_le_char();
     printf("%c\n", letra);
     printf("%c\n", vetPalavras[palavra_selecionada].palavra[0]);
 
-    if (palavra_selecionada == -1 && letra!=0)
+    if (palavra_selecionada == -1 && letra != 0)
     {
       palavra_selecionada = encontra_palavra_com_letra(letra, 0);
     }
@@ -235,21 +265,49 @@ void jogo()
         palavra_selecionada = -1; // Permite selecionar uma nova palavra
       }
     }
-  
-    if (quantidade_de_palavras_acertadas == TOTAL_DE_PALAVRAS || testa_tempoDigitacao(vetPalavras, t0))
+
+    // Verifica se a palavra foi toda acertada
+    if (vetPalavras[*palavra_selecionada].palavra[0] == '\0')
     {
-      tela_limpa();
-      tela_lincol(tela_nlin() / 2,tela_ncol() / 2);
-      if (quantidade_de_palavras_acertadas == TOTAL_DE_PALAVRAS) {
-        printf("Parabéns, você acertou todas as palavras!");
-      } else {
-        printf("Tempo Esgotado!");   
-      }
-      break;
-    } 
+      quantidade_de_palavras_acertadas++;
+      *letra = '0';
+      quantidade_de_palavras--;  // Diminui a contagem total de palavras.
+      *palavra_selecionada = -1; // Permite selecionar uma nova palavra
+    }
+
+    // Atualiza a pontuação
+    *pontos += calcular_pontos(tela_relogio(), inicio);
   }
+
+  else
+  {
+    // Desconta pontos por letra errada
+    *pontos -= des;
+    if (*pontos < 0)
+    {
+      *pontos = 0;
+    }
+  }
+
+  if (quantidade_de_palavras_acertadas == TOTAL_DE_PALAVRAS || testa_tempoDigitacao(vetPalavras, t0))
+  {
+    tela_limpa();
+    tela_lincol(tela_nlin() / 2, tela_ncol() / 2);
+    if (quantidade_de_palavras_acertadas == TOTAL_DE_PALAVRAS)
+    {
+      printf("Parabéns, você acertou todas as palavras!");
+    }
+    else
+    {
+      printf("Tempo Esgotado!");
+    }
+    break;
+  }
+
   tela_atualiza();
 }
+
+
 
 void espera_enter()
 {
@@ -335,4 +393,57 @@ bool testa_tempoDigitacao(palavras palavrasJogo[], double inicio)
     }
   }
   return false;
+}
+
+void atualizar_recorde(float pontos, const char *identificador)
+{
+  // Atualiza os recordes se necessário
+  for (int i = 0; i < 3; i++)
+  {
+    if (pontos > recordes[i].pontos)
+    {
+      for (int j = 2; j > i; j--)
+      {
+        recordes[j] = recordes[j - 1];
+      }
+      recordes[i].pontos = pontos;
+      strcpy(recordes[i].identificador, identificador);
+      break;
+    }
+  }
+}
+
+void carregar_recorde()
+{
+  FILE *arquivo = fopen("recordes.txt", "r");
+  if (arquivo == NULL)
+  {
+    // Caso o arquivo não exista, inicializa os recordes
+    for (int i = 0; i < 3; i++)
+    {
+      recordes[i].pontos = 0.0;
+      strcpy(recordes[i].identificador, "");
+    }
+    return;
+  }
+
+  // Lê os recordes do arquivo
+  fread(recordes, sizeof(ranking), 3, arquivo);
+
+  fclose(arquivo);
+}
+
+void salvar_recorde()
+{
+  FILE *arquivo = fopen("recordes.txt", "w");
+  if (arquivo == NULL)
+  {
+    perror("Erro ao abrir o arquivo de recordes para escrita");
+    return;
+  }
+
+  // Grava os recordes no arquivo
+  fwrite(recordes, sizeof(ranking), 3, arquivo);
+
+  fclose(arquivo);
 }
